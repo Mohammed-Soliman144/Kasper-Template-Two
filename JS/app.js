132f9dd84550b3258b80da4da2fa60a3740d1087
Mohammed-Soliman144 <mohammedsoliman144@gmail.com>
2025-10-06 14:01:47 +0300

    update JS file

--- JS/app.js
+++ JS/app.js
@@ -101,11 +101,11 @@ function setImgBackground(arrname, arrowname) {
             if(arrowname === 'left'){
                 if(currentIndex === -1) currentIndex = 0;
                 currentIndex = (currentIndex - 1 + arrname.length) % arrname.length;
-                const url = new URL(`/IMG/${arrname[currentIndex]}`,window.location.href);
+                const url = new URL(`IMG/${arrname[currentIndex]}`,window.location.href);
                 return `url(${url.href})`;
             } else if (arrowname === 'right'){
                 currentIndex = (currentIndex + 1) % arrname.length;
-                const url = new URL(`/IMG/${arrname[currentIndex]}`,window.location.href);
+                const url = new URL(`IMG/${arrname[currentIndex]}`,window.location.href);
                 return  `url(${url.href})`;
             }
         currentIndex++;
@@ -125,7 +125,7 @@ function updateSlider() {
     bulletsBtn[indexBullet = indexBullet === -1 ?  2 : indexBullet  % bulletsBtn.length].classList.add('active');
     // indexBullet++
     // bulletsBtn[currentSlider = currentSlider > 2 ? 0 : currentSlider < 0 ? 0 : currentSlider].classList.toggle('active');
-    const url = new URL(`/IMG/${Images[currentSlider]}`,window.location.href);
+    const url = new URL(`IMG/${Images[currentSlider]}`,window.location.href);
     return landingPage.style.backgroundImage = `url(${url.href})`
 }
 
@@ -286,7 +286,7 @@ function changeStateBtn(btnname) {
 function changeImage(){
     // change image
         skillImageBtns.forEach(img => {
-            const url = new URL(`/IMG/${skillsImg[currentImage]}`,window.location.href);
+            const url = new URL(`IMG/${skillsImg[currentImage]}`,window.location.href);
             img.setAttribute('src',`${url.href}`);
             // currentImage = currentImage === skillsImg.length - 1 ? 0  : currentImage + 1 % skillsImg.length;
             currentImage = currentImage === skillsImg.length - 1 ? 0  : currentImage + 1 % skillsImg.length;
